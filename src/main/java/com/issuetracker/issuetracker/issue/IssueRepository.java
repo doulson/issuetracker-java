0cbbf86f961f63f37565a688fba5f769318b875b
doulson <166425591+doulson5598@users.noreply.github.com>
2024-05-17 00:21:23 +0800

    update IssueRepository.java

--- src/main/java/com/issuetracker/issuetracker/issue/IssueRepository.java
+++ src/main/java/com/issuetracker/issuetracker/issue/IssueRepository.java
@@ -1,9 +1,19 @@
 package com.issuetracker.issuetracker.issue;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface IssueRepository extends JpaRepository<Issue, Integer>, JpaSpecificationExecutor<Issue> {
 
+    @Query("""
+            SELECT issue
+            FROM Issue issue
+            WHERE issue.archived = false
+            AND issue.owner.id != :userId
+            """)
+    Page<Integer> findAllDisplayableIssues(Pageable pageable, Integer userId);
 }
